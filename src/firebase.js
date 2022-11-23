998c16ecbb91e23201428260e747c1867ba2a6f4
Anton <santon84@gmail.com>
2022-11-23 21:50:23 +0300

    new project toDo

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDvgVVqkg6J6NmLR99eSeczjNBK4fCEVA",
+  authDomain: "todo-86b5f.firebaseapp.com",
+  projectId: "todo-86b5f",
+  storageBucket: "todo-86b5f.appspot.com",
+  messagingSenderId: "460705203671",
+  appId: "1:460705203671:web:35659b9cd4b8c87ecdb792"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
